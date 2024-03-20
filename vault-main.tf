3320d2d16c7c1af284e3052f804ca82724f6b7b8
shawnjotom <156146113+shawnjotom@users.noreply.github.com>
2024-03-20 10:36:57 -0400

    Create vault-main.tf

---
+++ vault-main.tf
@@ -0,0 +1,96 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+    vault = {
+      source = "hashicorp/vault"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+provider "vault" {
+  address = "http://127.0.0.1:8200"
+  token   = "hvs.LksRvL7AKKWO0pWxmRbs0ofE"
+}
+
+resource "aws_iam_role" "my-role" {
+  name = "my-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "example_attachment" {
+  role       = aws_iam_role.my-role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+}
+
+resource "aws_iam_instance_profile" "example_profile" {
+  name = "example_profile"
+  role = aws_iam_role.my-role.name
+}
+
+resource "aws_instance" "example_instance" {
+  ami           = "ami-06ca3ca175f37dd66"
+  instance_type = "t2.micro"
+  
+  iam_instance_profile = aws_iam_instance_profile.example_profile.name
+
+  tags = {
+    Name = "exampleinstance"
+  }
+}
+
+resource "aws_s3_bucket_policy" "example_bucket_policy" {
+  bucket = "example-bucket"
+
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "AWS": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${aws_iam_role.my-role.name}"
+        },
+        "Action": [
+          "s3:GetObject",
+          "s3:ListBucket"
+        ],
+        "Resource": [
+          "arn:aws:s3:::example-bucket",
+          "arn:aws:s3:::example-bucket/*"
+        ]
+      }
+    ]
+  })
+}
+
+data "aws_caller_identity" "current" {}
+
+# Example Vault secret engine integration
+resource "vault_generic_secret" "example_secret" {
+  path = "secret/example"
+  data_json = jsonencode({
+    username = "my_username",
+    password = "my_password"
+  })
+}
